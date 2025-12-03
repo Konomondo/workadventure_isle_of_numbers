189ae17d79ae4d2a22653ba0ecf2750c619d3ac6
Gerhard Dorn <67096719+dorn-gerhard@users.noreply.github.com>
2025-12-03 15:52:02 +0100

    adjust packages

--- [Base]BaseChip_pipo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="[Base]BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1792" columns="16">
- <image source="[Base]BaseChip_pipo.png" width="512" height="3600"/>
-</tileset>
